9d76b60c8825bf692718c175187293862a5c78b3
hieuphpc07025 <144969739+hieuphpc07025@users.noreply.github.com>
2024-07-28 17:27:06 +0700

    [Frontend] - Home

---
+++ Web_DongHo_WebAssembly/Pages/Home/Index.razor.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Web_DongHo_WebAssembly.Data;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Web_DongHo_WebAssembly.Pages.Home
+{
+    public partial class Index : ComponentBase
+    {
+        [Inject]
+        private HttpClient http { get; set; }
+
+        private HomeProductRequest homeProductRequest = new HomeProductRequest();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetProduct();
+        }
+        public async Task GetProduct()
+        {
+            try
+            {
+                var response = await http.GetAsync("https://localhost:44355/api/Home/homeProducts");
+                response.EnsureSuccessStatusCode();
+
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                homeProductRequest = JsonConvert.DeserializeObject<HomeProductRequest>(jsonResponse);
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Request error: {ex.Message}");
+            }
+            catch (System.Text.Json.JsonException ex)
+            {
+                Console.WriteLine($"JSON error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+            }
+        }
+    }
+    public class HomeProductRequest
+    {
+        public List<Product> Productfirst8 { get; set; }
+        public List<Product> Productsecond8 { get; set; }
+        public List<Product> Productthird8 { get; set; }
+    }
+}
